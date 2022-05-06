08bba0072c52008fb4be5c31c6d573fbd34098fd
gbcbooks <gbcbooksmj@gmail.com>
2022-05-07 00:37:56 +0800

    update

--- collect_data.sh
+++ collect_data.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
+
+ #default path
 path_dir=/usr/local/scripts/auto-bgp-correct
 cd $path_dir
-mthread() {
-        tmpfifo="/tmp/$$.fifo"
-        mkfifo $tmpfifo
-        exec 4<>$tmpfifo
-        rm $tmpfifo
-        thread=15
-        for ((i=0;i<$thread;i++));
-        do
-                echo ""
-        done >&4
-
-}
-
 
 main() {
-mthread
-clock=$(date +%Y-%m-%d\ %H:%M:%S)
-for i in $(cat $path_dir/target)
-do
-        read -u4
-        {
-#       echo "$clock $(ping $i -c 100 -i 0.01 -q 2>&1 | grep "packet loss" | cut -f 6 -d " " | tr "%" " ")" >> $path_dir/db/${i}.log
-        echo "$clock $(ping $i -c 100 -i 0.01 -q 2>&1 | grep -E "(packet loss|rtt)" | xargs | cut -d " " -f 6,14 | sed "s/%//;s/\// /g" | cut -d " " -f 1,3)" >> $path_dir/db/${i}.log
-        } &
-done 
-
+    init
+    clock=$(date +%Y-%m-%d\ %H:%M:%S)
+    alltargats=$(cat target | sort | uniq | xargs )
+    alldata=$(fping ${alltargats} -c 100 -i 1 -p 10 -t 1000 -q 2>&1)
+    for item in $(cat $path_dir/target)
+    do
+        itemdata=$(echo "${alldata}" | grep ${item} | sed "s/,/\//;s/\// /g;s/%//g;s/ \+/ /g" | cut -d " " -f 9,15)
+        echo "itemdata is : ${itemdata}"
+        echo "$clock ${itemdata}" >> $path_dir/db/${item}.log
+    done
 }
-main
+main $*
